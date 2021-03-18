3543853dc54051ea59f591c1cf194a2144f6c6e2
h118424 <Kiran.Kandula@Honeywell.com>
2021-03-18 19:24:14 +0530

    Authentication using passport with Google OAuth20

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const path = require('path')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
+const reload = require('livereload')
 
 const connectDB = require('./config/db')
 
 //load config
 dotenv.config({path: './config/config.env'})
+
+// Passport config
+require('./config/passport')(passport)
 
 connectDB() 
 
@@ -28,13 +34,29 @@ app.engine(
   )
 app.set('view engine', '.hbs')
 
+
+// Sessions
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+  })
+)
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 //Set Static
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 3003
 
 app.listen(PORT, console.log(`Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`))
 
+// reload(app)
